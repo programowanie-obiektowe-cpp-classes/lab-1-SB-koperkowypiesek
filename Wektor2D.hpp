08c45449bd8aa1104b06a4e46862e223e171fb35
SB-koperkowypiesek <76916080+SB-koperkowypiesek@users.noreply.github.com>
2022-10-26 23:33:13 +0200

    2nd try

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,9 +1,8 @@
 class Wektor2D
 {
 private:
-    double     x;
-    double     y;
-    static int num_wek;
+    double x;
+    double y;
 
 public:
     Wektor2D(double x0, double y0)
@@ -11,7 +10,6 @@ public:
         x = x0;
         y = y0;
         std::cout << "nowy wektor [" << x << ", " << y << "]\n";
-        ++num_wek;
     }
 
     Wektor2D()
@@ -20,11 +18,7 @@ public:
         y = 0;
     }
 
-    ~Wektor2D()
-    {
-        std::cout << "zegnam [" << x << ", " << y << "]\n";
-        --num_wek;
-    }
+    ~Wektor2D() { std::cout << "zegnam [" << x << ", " << y << "]\n"; }
 
     void   setX(double x0) { x = x0; }
     void   setY(double y0) { y = y0; }
@@ -33,7 +27,6 @@ public:
 
     void print() { std::cout << "x:" << x << "\ty: " << y << "\n"; }
     void norm() { std::cout << "norm: " << sqrt(x * x + y * y) << "\n"; }
-    int  populacja() { return num_wek; }
 
     Wektor2D operator+(Wektor2D w2) { return Wektor2D(x + w2.x, y + w2.y); }
     double   operator*(Wektor2D w2) { return x * w2.x + y * w2.y; }
